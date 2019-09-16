a582fcae80084f4ac38d3074e94ba495d8bf485c
Cameron <cameronjmaccabe@gmail.com>
2019-09-15 21:47:17 -0700

    added sliding image functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,17 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const slidingImages = document.querySelectorAll(".slide-in");
+
+function checkSlide() {
+  slidingImages.forEach(image => {
+    const slideInAt = (window.scrollY + window.innerHeight) - image.height / 2;
+    const imageBottom = image.offsetTop + image.height;
+    const isHalfShown = slideInAt > image.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    isHalfShown && isNotScrolledPast ? image.classList.add("active") : image.classList.remove("active");
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
